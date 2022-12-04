1b0569f966bf2f5d89c879aacc5204123bcb63b9
Maximilian Rex Alba <albam476@gmail.com>
2022-12-04 18:38:25 -0500

    commit tree 3eb8f370eefe1a4c428f1c4c12d7b9020885b727

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator name_arr
+    name_arr.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms speaker_arr
+
+    speaker_arr.map.with_index(1) do |name , i|
+        "Hello, #{name}! You'll be assigned to room #{i}!"
+    end
+end
+
+def printer (speaker_arr)
+    speaker_arr.each do |name|
+        puts badge_maker(name)
+    end
+
+    assign_rooms(speaker_arr).each do |greeting|
+        puts greeting
+    end
+    
+end
